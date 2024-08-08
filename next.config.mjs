9c58a9f13cb1c4dd4df10e13f1805fcb64ac262b
Daniel Pérez <danielperez@Daniels-MacBook-Pro.local>
2024-08-07 22:12:27 -0600

    Update next.config.mjs, package-lock.json, and package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['143.198.158.94'], // Añade tu IP aquí
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '143.198.158.94',
+      },
+    ],
   },
 };
 
