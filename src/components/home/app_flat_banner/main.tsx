ac0cd72a31b49fc93c351ddfececff3f95fd37ff
Daniel Perez <danielps@setenal.mx>
2024-08-30 13:03:52 -0600

    Update main.tsx, main.tsx, and main.tsx

--- src/components/home/app_flat_banner/main.tsx
+++ src/components/home/app_flat_banner/main.tsx
@@ -3,12 +3,30 @@ import Carousel from "react-material-ui-carousel";
 import Banner from "./banner";
 import { Banner as BannerInterface} from "@/service/banners/interface";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function AppFlatBanner({banners}:{banners:BannerInterface[]}){
     const router = useRouter();
-    console.log(banners)
+    const [carouselKey, setCarouselKey] = useState(Date.now());
+
+    useEffect(() => {
+        function handleResize() {
+            // Forzar una actualización del carrusel
+            setCarouselKey(Date.now());
+        }
+
+        // Añadir el event listener para resize
+        window.addEventListener('resize', handleResize);
+
+        // Llamar a handleResize al montar el componente para ajustar el tamaño
+        handleResize();
+
+        // Limpiar el event listener al desmontar el componente
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return(
-    <Carousel >
+    <Carousel key={carouselKey}>
      {banners.filter(banner => banner.assetUrl).map((banner, i)=><Banner key={`flat_banner_${i}`} content={banner.assetUrl!} onClick={() => window.open('https://www.youtube.com', '_blank', 'noopener,noreferrer')}></Banner>)
         
     }
--- src/components/home/app_principal_banner/main.tsx
+++ src/components/home/app_principal_banner/main.tsx
@@ -3,11 +3,29 @@ import Carousel from "react-material-ui-carousel";
 import Banner from "./banner";
 import { Banner as BannerInterface} from "@/service/banners/interface";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function AppPrincipalBanner({banners}:{banners:BannerInterface[]}){
     const router = useRouter();
+    const [carouselKey, setCarouselKey] = useState(Date.now());
+
+    useEffect(() => {
+        function handleResize() {
+            // Forzar una actualización del carrusel
+            setCarouselKey(Date.now());
+        }
+
+        // Añadir el event listener para resize
+        window.addEventListener('resize', handleResize);
+
+        // Llamar a handleResize al montar el componente para ajustar el tamaño
+        handleResize();
+
+        // Limpiar el event listener al desmontar el componente
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
     return(
-    <Carousel >
+    <Carousel key={carouselKey}>
     {banners?.filter(banner => banner.assetUrl).map((banner, i)=><Banner key={`mega_banner_${i}`} content={banner.assetUrl!} onClick={banner.link ? () => router.push(banner.link!): undefined}></Banner>)
         
         }
