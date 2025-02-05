884a62736ca1207fa8009f704985f52758688766
dan120929 <dan.salgado58d@gmail.com>
2025-02-04 18:30:04 -0600

    Actualizar main.tsx

--- src/components/common/app_nav_bar/main.tsx
+++ src/components/common/app_nav_bar/main.tsx
@@ -159,6 +159,7 @@ export default function AppNavBar({}) {
                       <MenuItem
                         onClick={() => {
                           localStorage.clear();
+                          setIsLocalStorage(false);
                           handleClose();
                         }}
                       >
