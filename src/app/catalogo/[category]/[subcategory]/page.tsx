0498fb7e9a99bcddac9d4bffed8f1b03d355ca23
dan120929 <dan.salgado58d@gmail.com>
2025-02-19 08:07:38 -0600

    Actualizar page.tsx y store.ts

--- src/app/catalogo/[category]/[subcategory]/page.tsx
+++ src/app/catalogo/[category]/[subcategory]/page.tsx
@@ -39,13 +39,20 @@ export default function Catalogo() {
 
   // Cargar más productos con paginación
   const loadMoreProducts = useCallback(async () => {
+    
     if (loading || !hasMore) return;
 
+    
     setLoading(true);
     const nextPage = page + 1;
 
-    if (selectedSubcategoria) {
-      await getSubcategoriaPaginada(selectedSubcategoria, nextPage);
+    if (subcategoriaPaginada) {
+      console.log("hola")
+      setLoading(true);
+      await getSubcategoriaPaginada(subcategoriaPaginada.datosPaginados.id, nextPage);
+      setLoading(false)
+      
+      setPage(nextPage);
     } else {
       setPage(nextPage);
     }
